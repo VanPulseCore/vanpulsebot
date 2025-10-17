af8e3d848c0613852864da458862d89b8f4b6bbf
VanPulseCore <pradasod@gmail.com>
2025-10-17 16:50:29 +0200

    Amélioration de /annonce avec sauts de ligne

--- commands/announce.js
+++ commands/announce.js
@@ -4,17 +4,17 @@ const vanpulseEmbed = require('../utils/embed');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('annonce')
-    .setDescription('Publie une annonce stylisée')
+    .setDescription('Publie une annonce stylée')
     .addStringOption(opt =>
       opt.setName('titre').setDescription('Titre de l’annonce').setRequired(true))
     .addStringOption(opt =>
       opt.setName('contenu').setDescription('Contenu de l’annonce').setRequired(true)),
+
   async execute(interaction) {
     const titre = interaction.options.getString('titre');
-    const contenu = interaction.options.getString('contenu');
+    const contenu = interaction.options.getString('contenu').replace(/\\n/g, '\n');
 
-    const embed = vanpulseEmbed(`📣 ${titre}`, contenu);
-
+    const embed = vanpulseEmbed(titre, contenu);
     await interaction.reply({
       embeds: [embed],
       files: ['./assets/logo.png', './assets/banner.png']
